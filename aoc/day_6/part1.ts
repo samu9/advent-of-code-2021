3945f1fb5e792a4652e2295c28a47fd5ba0033ba
Samuele <s.fusaro@geniodiligence.it>
2021-12-06 22:11:19 +0100

    start day 6

---
+++ aoc/day_6/part1.ts
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const path = require("path")
+
+const input_: string[] = fs
+    .readFileSync(path.join(path.dirname(__filename), "input.txt"), "utf-8")
+    .split(",")
+
+const SON_CYCLE = 7
+
+const lanternfishLife = (start: number, days: number) => {
+    let internal_timer = start
+    let sons = 
+}
+
+let count = input_.length
+
+console.log(input_)
+
+input_.forEach((i) => {
+
+})
+
+export {}
