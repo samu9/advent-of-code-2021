d27ba0389bed075c86f3555583e0691b538a0b4d
Samuele <s.fusaro@geniodiligence.it>
2021-12-10 16:41:10 +0100

    day 10

---
+++ aoc/day_10/part1.py
@@ -0,0 +1,44 @@
+from utils import read_input
+
+input_ = read_input(__file__)
+
+closer = {
+    "(": ")",
+    "[": "]",
+    "<": ">",
+    "{": "}",
+}
+
+points = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+
+
+def navigate(index, line):
+    global points_sum
+    char = line[index]
+
+    next = index + 1
+    while line[next] in closer.keys():
+        # print(next)
+        next = navigate(next, line)
+        if not next:
+            return False
+
+    if line[next] != closer[char]:
+        if line[next] not in closer.values():
+            points_sum += points[line[next]]
+        return False
+    return next + 1
+
+points_sum = 0
+for i, line in enumerate(input_):
+    for index, c in enumerate(line):
+        try:
+            navigate(index, line)
+        except:
+            pass
+print(points_sum)
