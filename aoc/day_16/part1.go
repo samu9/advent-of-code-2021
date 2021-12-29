9771a99b295ae52622c9d2a122a620f4dc1e86c9
Samuele <s.fusaro@geniodiligence.it>
2021-12-29 09:53:59 +0100

    day 16

---
+++ aoc/day_16/part1.go
@@ -0,0 +1,17 @@
+package day16
+
+import (
+	"aoc/utils"
+	"fmt"
+)
+
+var input = utils.ReadInput("aoc/day_16/input.txt")[0]
+
+func Main() {
+
+	l := input[0]
+	fmt.Println(l)
+	// c, _ := hex.Encode()
+	// fmt.Println(l, c)
+	// fmt.Println(l, string(l), hex.En, ok)
+}
